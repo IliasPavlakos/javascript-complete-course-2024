620cdca7fc8632aad83318f0a913383a28851963
ipavlakos <pavlakosIlias@outlook.com>
2024-09-25 21:22:58 +0300

    01: Data types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,9 +1,29 @@
+// let firstName = 'Ilias';
+// let lastName = 'Pavlakos';
+//
+// console.log(firstName + ' ' + lastName);
+//
+// let PI = Math.PI;
+// console.log(PI);
 
+let aNumberVar = 1;
+let aStringVar = 'Hello World';
+let aBooleanVar = true;
+let aUndefinedVar = undefined;
+let aNullVar = null;
 
-let firstName = 'Ilias';
-let lastName = 'Pavlakos';
+// A single line comment
 
-console.log(firstName + ' ' +  lastName);
+/*
+A multiline comment
+*/
 
-let PI = Math.PI;
-console.log(PI);
+console.log(typeof aNumberVar);
+console.log(typeof aStringVar);
+console.log(typeof aBooleanVar);
+console.log(typeof aUndefinedVar);
+console.log(typeof aNullVar); // return object with is a known bug
+
+aNumberVar = '1';
+
+console.log(typeof aNumberVar); // changed to string
