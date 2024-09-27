394bc7cb65a114c1081bd2456333e2ddec607da5
ipavlakos <pavlakosIlias@outlook.com>
2024-09-27 22:02:58 +0300

    02: Array operations

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -41,3 +41,26 @@
 //
 // console.log(fruits.length);
 
+// const fruits = ['banana', 'apple', 'mango'];
+// console.log(fruits);
+//
+// const coconutIndex = fruits.push('coconut') - 1;
+// console.log(fruits);
+//
+// console.log('Added ' + fruits[coconutIndex]);
+//
+// fruits.unshift('cherry');
+// console.log(fruits);
+//
+// const popped = fruits.pop();
+// console.log(`popped ${popped}`);
+// console.log(fruits);
+//
+//
+// fruits.shift();
+// console.log(fruits);
+//
+// console.log('apple is at index ' + fruits.indexOf('apple'));
+//
+// console.log(fruits.includes('apple'));
+// console.log(fruits.includes('cherry'));
