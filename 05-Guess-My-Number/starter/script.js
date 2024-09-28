57d202bc6502545f873ad5024035f2ec722730f0
ipavlakos <pavlakosIlias@outlook.com>
2024-09-28 10:44:55 +0300

    05: handling click events

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,8 +1,18 @@
 'use strict';
+//
+// document.querySelector('.message').textContent = 'Correct Number!';
+// document.querySelector('.number').textContent = '35';
+// document.querySelector('.score').textContent = '35';
+//
+// document.querySelector('.guess').value = 35;
+//
 
-document.querySelector('.message').textContent = 'Correct Number!';
-document.querySelector('.number').textContent = '35';
-document.querySelector('.score').textContent = '35';
+document.querySelector('.check').addEventListener('click', function(){
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess);
 
-document.querySelector('.guess').value = 35;
-
+    if(!guess){
+        document.querySelector('.message').textContent = 'No number!';
+        document.querySelector('.guess').focus();
+    }
+})
