51ec112a551bc81b4a478a5efe7c8a21df364705
ipavlakos <pavlakosIlias@outlook.com>
2024-09-28 10:38:29 +0300

    05: manipulating dom elements

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,4 +1,8 @@
 'use strict';
 
-let message = document.querySelector('.message').textContent;
-console.log(message);
+document.querySelector('.message').textContent = 'Correct Number!';
+document.querySelector('.number').textContent = '35';
+document.querySelector('.score').textContent = '35';
+
+document.querySelector('.guess').value = 35;
+
