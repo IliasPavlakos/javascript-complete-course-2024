ddc68cf4eaec4571caa1125f028fc5879e40c970
ipavlakos <pavlakosIlias@outlook.com>
2024-09-28 11:28:57 +0300

    06: closing modal when clicking outside it and using functions

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -6,15 +6,21 @@ const btnShowModal = document.querySelectorAll('.show-modal');
 const btnClosedModal = document.querySelector('.close-modal');
 
 for (let i = 0; i < btnShowModal.length; i++) {
-    btnShowModal[i].addEventListener('click', () => {
-        modal.classList.remove('hidden');
-        overlay.classList.remove('hidden');
-    });
+    btnShowModal[i].addEventListener('click', openModal);
 }
 
-btnClosedModal.addEventListener('click', () => {
+btnClosedModal.addEventListener('click', closeModal)
+overlay.addEventListener('click', closeModal);
+
+function openModal(){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+function closeModal(){
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
-})
+}
+
 
 
