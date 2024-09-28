cd192e569347d6c3420f200fbc444bdf1c9fb567
ipavlakos <pavlakosIlias@outlook.com>
2024-09-28 11:26:55 +0300

    06: modal overlay

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -8,11 +8,13 @@ const btnClosedModal = document.querySelector('.close-modal');
 for (let i = 0; i < btnShowModal.length; i++) {
     btnShowModal[i].addEventListener('click', () => {
         modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
     });
 }
 
 btnClosedModal.addEventListener('click', () => {
     modal.classList.add('hidden');
+    overlay.classList.add('hidden');
 })
 
 
