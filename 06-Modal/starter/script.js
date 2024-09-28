156acd9fdb4c635db9562e622d7f77c60b0fcf1c
ipavlakos <pavlakosIlias@outlook.com>
2024-09-28 11:19:57 +0300

    06: modal window selecting dom elements and querySelector

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const showModal = document.querySelectorAll('.show-modal');
+const closedModal = document.querySelector('.closed');
+
+
