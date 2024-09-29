bd3604a3548fe9b74fe64ed62aae928ecffadd49
ipavlakos <pavlakosIlias@outlook.com>
2024-09-29 12:15:34 +0300

    10: function default parameters

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const bookings = [];
+
+const createBooking  = function (flightNumber,PassengersCounts = null,price = null){
+    const booking = {
+        flightNumber,
+        PassengersCounts,
+        price
+    };
+
+    bookings.push(booking);
+    console.log(booking);
+}
+
+createBooking('LH123');
+createBooking('LH123', 47, 85);
